f41e74e1ddf34210a19ec12d80dd1cfc953688ca
Omar Girones <oborras@MacBook-Air-de-Omar.local>
2026-02-09 23:23:29 +0100

    fix: remove prisma.config.ts (vercel incompatible)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
 };
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
+    log: ["error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") {
